fd3eff8271374aeb4f8afd762a06ecb57c205ba0
gourav <gouravmaallah@gmail.com>
2023-05-19 13:00:57 -0400

    complete assignment 1

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -1,78 +1,52 @@
-﻿namespace Assignment
+﻿using System;
+
+public class Program
 {
-    public static class ArrayReplicator
+    public static int AskForNumber(string text)
     {
-        /// <summary>
-        /// Replicates (deep copies) the incoming array
-        /// </summary>
-        /// <param name="original">The array to be replicated</param>
-        /// <returns>A deep copy of the original array</returns>
-        public static int[] ReplicateArray(int[] original)
-        {
-            int size = original.Length;
-            int[] copyArray = new int[size];
-            // LINQ -> map
-            for (int i = 0; i < size; ++i)
-            {
-                copyArray[i] = original[i];
-            }
-            return copyArray;
-        }
-
-        /// <summary>
-        /// Asks the user for a number. Will crash if the user input is not convertible to an int (throw exception?)
-        /// </summary>
-        /// <param name="text">Text to prompt the user</param>
-        /// <returns>The user input as an integer</returns>
-        public static int AskForNumber(string text)
-        {
-            Console.Write(text);
-            string userInput = Console.ReadLine(); // Needs work -> try/catch + loop or Int.TryParse + loop
-            int number = Convert.ToInt32(userInput);
-            return number;
-        }
-
-        /// <summary>
-        /// Asks the user for a number within a certain range [min, max]. If the number is not in the range, re-prompt the user for a new number.
-        /// Will crash if the user input is not convertible to an int (throw exception?)
-        /// </summary>
-        /// <param name="text">Text to prompt the user</param>
-        /// <param name="min">Smallest permissible value</param>
-        /// <param name="max">Largest permissible value</param>
-        /// <returns>The user input as an integer</returns>
-        public static int AskForNumber(string text, int min, int max)
-        {
-            int userInput = AskForNumber(text);
-            // While the user input is outside the expected range
-            while (userInput < min || userInput > max)
-            {
-                userInput = AskForNumber("Your previous input is not valid, try again.");
-            }
-            return userInput;
-        }
+        Console.WriteLine(text);
+        string input = Console.ReadLine();
+        int number = Convert.ToInt32(input);
+        return number;
     }
 
-    static class Program
+    public static int AskForNumberInRange(string text, int min, int max)
     {
-        static void Main()
+        int number;
+        do
         {
-            const int Min = 0;
-            const int Max = 10;
-            const int PrintOffset = 4;
+            number = AskForNumber(text);
+        } while (number < min || number > max);
 
-            int size = ArrayReplicator.AskForNumber("Enter the array size: ", Min, Max);
-            int[] original = new int[size];
+        return number;
+    }
 
-            // Fill the original array with user specified integers
-            for (int item = 0; item < size; ++item)
-            {
-                original[item] = ArrayReplicator.AskForNumber("Enter a number: ");
-            }
+    public static int[] ReplicateArray(int[] original)
+    {
+        int[] replicatedArray = new int[original.Length];
+        for (int i = 0; i < original.Length; i++)
+        {
+            replicatedArray[i] = original[i];
+        }
+        return replicatedArray;
+    }
 
-            int[] copy = ArrayReplicator.ReplicateArray(original);
-            // Verify original and replicated array are the same
-            for (int index = 0; index < size; ++index)
-                Console.WriteLine($"Original {original[index],-PrintOffset}  {copy[index],4} Copy");
+    public static void Main(string[] args)
+    {
+        int size = AskForNumberInRange("Enter the size of the array (0-5):", 0, 5);
+        int[] userArray = new int[size];
+
+        for (int i = 0; i < size; i++)
+        {
+            userArray[i] = AskForNumber($"Enter element #{i + 1}:");
+        }
+
+        int[] replicatedArray = ReplicateArray(userArray);
+
+        Console.WriteLine("Copied array:");
+        foreach (int num in replicatedArray)
+        {
+            Console.WriteLine(num);
         }
     }
 }
